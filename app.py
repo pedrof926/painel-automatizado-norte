dbd1e5ebde5d1542b165214737d9bc298bc4051b
pedrof926 <piurosfernandes@gmail.com>
2025-07-16 08:19:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -200,7 +200,11 @@ def update_graph(clickData):
 
     return fig, info_text
 
+app = dash.Dash(__name__)
+server = app.server
+
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 8050))
     app.run_server(host='0.0.0.0', port=port, debug=True)
 
